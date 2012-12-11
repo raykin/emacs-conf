9dd6398a9ed1abe8abe3d05655288cd756411d75
raykin <raykincoldxiao@gmail.com>
2012-12-11 18:25:57 +0800

    remove abbrev-mode binding from rhtml-mode cause abbrev-mode conflict with yas in rhtml-mode

--- plugin/rhtml/rhtml-mode.el
+++ plugin/rhtml/rhtml-mode.el
@@ -5,9 +5,9 @@
 ;; ***** BEGIN LICENSE BLOCK *****
 ;; Version: MPL 1.1/GPL 2.0/LGPL 2.1
 
-;; The contents of this file are subject to the Mozilla Public License Version 
-;; 1.1 (the "License"); you may not use this file except in compliance with 
-;; the License. You may obtain a copy of the License at 
+;; The contents of this file are subject to the Mozilla Public License Version
+;; 1.1 (the "License"); you may not use this file except in compliance with
+;; the License. You may obtain a copy of the License at
 ;; http://www.mozilla.org/MPL/
 
 ;; Software distributed under the License is distributed on an "AS IS" basis,
@@ -49,7 +49,6 @@
   html-mode "RHTML"
   "Embedded Ruby Mode (RHTML)"
   (interactive)
-  (abbrev-mode)
   ;; disable if you don't want it...
   (rhtml-activate-fontification))
 
