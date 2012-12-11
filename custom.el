69eda861d113e34132775c165cb4e1dcc849269d
raykin <raykincoldxiao@gmail.com>
2012-12-11 13:33:46 +0800

    add fs yas. fs map to ', focus => true'

--- custom.el
+++ custom.el
@@ -12,3 +12,8 @@
 (require 'session)
 (add-hook 'after-init-hook 'session-initialize)
 (setq desktop-globals-to-save '(desktop-missing-file-warning))     
+
+;; Add new yas
+
+(yas/define-snippets 'ruby-mode
+                     '(("fs" ", :focus => true " ", focus => true" nil "control structure" nil nil nil)))
