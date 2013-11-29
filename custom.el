200ebe7fa1e5bb97a502ce5e817bd77defbc03aa
raykin <raykincoldxiao@gmail.com>
2013-11-29 10:11:21 +0800

    add transparent script

--- custom.el
+++ custom.el
@@ -72,3 +72,17 @@
 
 ;; GUI
 (set-face-attribute 'default nil :height 170)
+
+;; Add Transparency script
+(set-frame-parameter (selected-frame) 'alpha '(85 50))
+(add-to-list 'default-frame-alist '(alpha 85 50))
+
+(eval-when-compile (require 'cl))
+(defun toggle-transparency ()
+  (interactive)
+  (if (/=
+       (cadr (frame-parameter nil 'alpha))
+       100)
+      (set-frame-parameter nil 'alpha '(100 100))
+    (set-frame-parameter nil 'alpha '(85 50))))
+(global-set-key (kbd "C-c t") 'toggle-transparency)
