a005f2ed44d96a6b0ed8db4170cf408f54997516
raykin <raykincoldxiao@gmail.com>
2013-12-05 09:57:51 +0800

    tmply remove rinari in elpa page init list

--- init.el
+++ init.el
@@ -32,7 +32,7 @@
               ))
 
 ;; Add in your own as you wish:
-(defvar my-packages '(starter-kit starter-kit-lisp starter-kit-bindings starter-kit-js starter-kit-ruby yasnippet-bundle rinari)
+(defvar my-packages '(starter-kit starter-kit-lisp starter-kit-bindings starter-kit-js starter-kit-ruby yasnippet-bundle)
   "A list of packages to ensure are installed at launch.")
 
 (dolist (p my-packages)
