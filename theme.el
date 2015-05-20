64e346ec85485ee3660ce01c56118dca85def9a8
raykin <raykincoldxiao@gmail.com>
2015-05-20 16:35:49 +0800

    add theme.el

---
+++ theme.el
@@ -0,0 +1,15 @@
+;; (require 'color-theme-sanityinc-tomorrow)
+
+(color-theme-railscasts)
+;;(load-theme 'soft-morning t)
+;;(load-theme 'zenburn t)
+;;(load-theme 'wombat)
+;;(load-theme 'solarized t)
+
+;; (color-theme-sanityinc-tomorrow-day)
+;; (color-theme-sanityinc-tomorrow-night)
+;; (color-theme-sanityinc-tomorrow-blue)
+;; (color-theme-sanityinc-tomorrow-bright)
+;; (color-theme-sanityinc-tomorrow-eighties)
+
+;; M-x customize-theme to change theme
