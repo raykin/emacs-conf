191cbe8dd46ce5f57af3d46017d8ad1e43f35440
raykin <raykincoldxiao@gmail.com>
2016-02-29 11:00:31 +0800

    get ENV from shell

--- init.el
+++ init.el
@@ -158,3 +158,7 @@
  ;; If there is more than one, they won't work right.
  '(mode-line ((t (:foreground "#030303" :background "#bdbdbd" :box nil))))
  '(mode-line-inactive ((t (:foreground "#f9f9f9" :background "#666666" :box nil)))))
+
+
+(getenv "PATH")
+(getenv "STUDIO")
