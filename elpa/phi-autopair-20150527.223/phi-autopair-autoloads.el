7c824a9cc4c533e81caef81ce47f57bc5ac1b046
raykin <raykincoldxiao@gmail.com>
2016-03-04 14:20:03 +0800

    remove elpa packages

--- elpa/phi-autopair-20150527.223/phi-autopair-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; phi-autopair-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("phi-autopair.el") (22229 46471 429993
-;;;;;;  0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; phi-autopair-autoloads.el ends here
--- elpa/phi-autopair-20150527.223/phi-autopair-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "phi-autopair" "20150527.223" "another simple-minded autopair implementation" '((paredit "20")) :url "http://hins11.yu-yake.com/")
