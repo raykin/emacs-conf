b5df07570b75a285ac8bbc59ace1d67e8fbe8bcc
raykin <raykincoldxiao@gmail.com>
2019-09-08 23:58:00 +0800

    update projectile index mode

--- init.el
+++ init.el
@@ -73,7 +73,8 @@
                       rainbow-mode ;; give color on css file. need background color to be white
                       sass-mode
                       js2-mode
-											typescript-mode
+											tide
+											company ;; enhance tide
                       )
   )
 
@@ -187,7 +188,7 @@
 		("5dc0ae2d193460de979a463b907b4b2c6d2c9c4657b2e9e66b8898d2592e3de5" "98cc377af705c0f2133bb6d340bf0becd08944a588804ee655809da5d8140de6" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "3b0a350918ee819dca209cec62d867678d7dac74f6195f5e3799aa206358a983" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088" default)))
  '(package-selected-packages
 	 (quote
-		(elixir-mode scss-mode enh-ruby-mode ws-butler rspec-mode rubocop lua-mode emojify material-theme color-theme-sanityinc-tomorrow dracula-theme nginx-mode typescript-mode web-mode rjsx-mode projectile-rails yaml-mode textmate sws-mode swiper smex smart-tab slim-mode sass-mode ruby-end robe rbenv rainbow-mode railscasts-theme quickrun projectile phi-autopair multiple-cursors mode-compile minitest markdown-mode magit load-relative key-chord json-mode jade-mode iy-go-to-char ido-yes-or-no ido-ubiquitous idle-highlight-mode hlinum find-file-in-project exec-path-from-shell csv-mode coffee-mode better-defaults aggressive-indent ag))))
+		(company tide elixir-mode scss-mode enh-ruby-mode ws-butler rspec-mode rubocop lua-mode emojify material-theme color-theme-sanityinc-tomorrow dracula-theme nginx-mode web-mode rjsx-mode projectile-rails yaml-mode textmate sws-mode swiper smex smart-tab slim-mode sass-mode ruby-end robe rbenv rainbow-mode railscasts-theme quickrun projectile phi-autopair multiple-cursors mode-compile minitest markdown-mode magit load-relative key-chord json-mode jade-mode iy-go-to-char ido-yes-or-no ido-ubiquitous idle-highlight-mode hlinum find-file-in-project exec-path-from-shell csv-mode coffee-mode better-defaults aggressive-indent ag))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -239,3 +240,6 @@
 (projectile-mode +1)
 (define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
 (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
+;; enable the usage of file ignore pattern
+(setq projectile-indexing-method 'native)
+(setq projectile-enable-caching t)
